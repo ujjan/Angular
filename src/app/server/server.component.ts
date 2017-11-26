6496ef2ea75b6f4ddb7e98525c2fdbe6b769936c
Ayaz Ali <ayaz.ujjan@gmail.com>
2017-11-26 23:56:44 +0100

    first comit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styles: [ `
+    p {
+        color: cornflowerblue;
+      }
+  `]
+})
+export class ServerComponent {
+
+}
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
